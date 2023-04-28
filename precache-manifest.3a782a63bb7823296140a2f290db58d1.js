a36a586e4c468f8ebbfd3a3e3b57c2ebe59d6719
SUNNY KUMAR <sunnykumark070@gmail.com>
2023-04-27 23:12:28 -0700

    Updates

---
+++ precache-manifest.3a782a63bb7823296140a2f290db58d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d842506fc0de0496738e",
+    "url": "/project/static/css/main.b7bf9a89.chunk.css"
+  },
+  {
+    "revision": "d842506fc0de0496738e",
+    "url": "/project/static/js/main.d842506f.chunk.js"
+  },
+  {
+    "revision": "2b4d80beaa6675769302",
+    "url": "/project/static/js/1.2b4d80be.chunk.js"
+  },
+  {
+    "revision": "e733734cfc14039553bf",
+    "url": "/project/static/js/runtime~main.e733734c.js"
+  },
+  {
+    "revision": "f78b0146986a898289fbc863d7c3320a",
+    "url": "/project/static/media/footer1.f78b0146.png"
+  },
+  {
+    "revision": "c655ed6c0fe849163d61957d419c39a5",
+    "url": "/project/static/media/creator.c655ed6c.svg"
+  },
+  {
+    "revision": "54b9c8518f1bfc8abe3f28634a6ebfb2",
+    "url": "/project/index.html"
+  }
+];
